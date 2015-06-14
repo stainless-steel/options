9ec1c32f7507e92cb88ad89b98cc46b0954c7ccd
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-14 10:57:09 -0400

    Split get into get and get_ref

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,14 @@ impl Options {
 
     /// Get the value of a parameter.
     #[inline]
-    pub fn get<T: Any>(&self, name: &str) -> Option<&T> {
+    pub fn get<T: Any + Clone>(&self, name: &str) -> Option<T> {
+        self.map.get(name).and_then(|ref value| value.downcast_ref::<T>())
+                          .and_then(|value| Some(value.clone()))
+    }
+
+    /// Get the value of a parameter by reference.
+    #[inline]
+    pub fn get_ref<T: Any>(&self, name: &str) -> Option<&T> {
         self.map.get(name).and_then(|ref value| value.downcast_ref::<T>())
     }
 
@@ -26,5 +33,51 @@ impl Options {
     pub fn set<'l, T: Any>(&'l mut self, name: &str, value: T) -> &'l mut Options {
         self.map.insert(name.to_string(), Box::new(value));
         self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use Options;
+
+    #[test]
+    fn get() {
+        macro_rules! test(
+            ($options:expr, $name:expr, $value:expr, $kind:ty) => (
+                assert_eq!($options.get::<$kind>($name).unwrap(), $value)
+            );
+        );
+
+        let options = setup();
+        test!(options, "a", 42, i32);
+        test!(options, "b", true, bool);
+        test!(options, "c", "Hi, there!", &str);
+        test!(options, "d", "Hello, world!".to_string(), String);
+    }
+
+    #[test]
+    fn get_ref() {
+        macro_rules! test(
+            ($options:expr, $name:expr, $value:expr, $kind:ty) => (
+                assert_eq!($options.get_ref::<$kind>($name).unwrap(), $value)
+            );
+        );
+
+        let options = setup();
+        test!(options, "a", &42, i32);
+        test!(options, "b", &true, bool);
+        test!(options, "c", &"Hi, there!", &str);
+        test!(options, "d", "Hello, world!", String);
+    }
+
+    fn setup() -> Options {
+        let mut options = Options::new();
+
+        options.set("a", 42)
+               .set("b", true)
+               .set("c", "Hi, there!")
+               .set("d", "Hello, world!".to_string());
+
+        options
     }
 }
